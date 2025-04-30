935ff075004f3380e92c16c0fba00832a158b095
akshat_1074 <valaniakshat1074@gmail.com>
2025-05-01 00:52:16 +0530

    Navbar

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,50 @@
+import React,{useState,useEffect} from 'react'
+
+const NavBar = () => {
+    const [scrolled,setScrolled] = useState(false);
+
+    useEffect(()=>{
+        const handleScroll=()=>{
+            const isScrolled = window.scrollY > 10;
+            setScrolled(isScrolled)
+        };
+
+        window.addEventListener("scroll",handleScroll);
+      
+      return()=>window.removeEventListener("scroll",handleScroll);
+
+    },[]);
+    
+  return (
+   <header className={`navbar ${scrolled ? "scrolled":"not-scrolled"} `}>
+        <div className='inner'>
+             <a href='#hero'>
+                Portfolio
+             </a>
+
+             <nav className='desktop'>
+                <ul>
+                    {navLinks.map(({link,name})=>(
+                        <li className='group' key={name}>
+                            <a href={link}>
+                               <span>{name}</span>
+                               <span className='underline'/>
+                            </a>
+                        </li>
+                    ))}
+                </ul>
+             </nav>
+
+             <a href="#contact" className='contact-btn group'>
+                <div className='inner'>
+                   <span>
+                      Contact me
+                   </span>
+                </div>
+             </a>
+        </div>
+   </header>
+)
+}
+
+export default NavBar
